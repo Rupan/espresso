da6bd39e674536c2f74864e0ec5e25ba2fcbfb2c
Michael Mohr <akihana@gmail.com>
2013-12-11 21:02:51 -0800

    Verify that the executable exists and is the correct size

--- src/net/temerity/espresso/Espresso.java
+++ src/net/temerity/espresso/Espresso.java
@@ -2,9 +2,11 @@ package net.temerity.espresso;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 import java.io.File;
 import java.io.InputStream;
 import java.io.FileOutputStream;
@@ -25,24 +27,28 @@ public class Espresso extends Activity
         // Context.getFilesDir():
         // Returns the absolute path to the directory on the filesystem
         // where files created with openFileOutput(String, int) are stored.
-        extract();
+        Context ctx = this.getApplicationContext();
+        if( extract() )
+          Toast.makeText(ctx, "Init complete", Toast.LENGTH_LONG).show();
+        else
+          Toast.makeText(ctx, "Failed to extract resources", Toast.LENGTH_LONG).show();
     }
 
     // extracts one of the embedded espresso binaries to the Files directory
-    private void extract() {
+    private boolean extract() {
       Context ctx = this.getApplicationContext();
       InputStream input = null;
       FileOutputStream output = null;
       File exe = new File(ctx.getFilesDir(), "/espresso");
+
       if( ! exe.exists() ) {
         try {
           input = ctx.getAssets().open(ABI1 + "/espresso");
           output = ctx.openFileOutput("espresso", Context.MODE_PRIVATE);
-          byte[] buffer = new byte[1024];
+          byte[] buffer = new byte[4096];
           int bytesRead;
           while( (bytesRead = input.read(buffer)) != -1 )
             output.write(buffer, 0, bytesRead);
-          exe.setExecutable(true);
         } catch( IOException ioe ) {
           Log.w(TAG, "Failed to extract espresso executble", ioe);
         } finally {
@@ -54,6 +60,25 @@ public class Espresso extends Activity
           }
         } // try
       } // if
-    } // extract()
 
+      if( exe.exists() ) {
+        try {
+          AssetFileDescriptor fd = ctx.getAssets().openFd(ABI1 + "/espresso");
+          long assetSize = fd.getLength();
+          fd.getParcelFileDescriptor().close();
+          if( exe.length() == assetSize ) {
+            if( ! exe.canExecute() ) exe.setExecutable(true);
+          } else {
+            exe.delete();
+            return false;
+          }
+        } catch( IOException ioe ) {
+          Log.w(TAG, "Verification failed", ioe);
+          return false;
+        }
+      } else {
+        return false;
+      }
+      return true;
+    } // extract()
 }
